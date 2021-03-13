292c8cbc79b9dcacf07e316fce8b9fead765cd22
MChung95 <markus_chung@hotmail.com>
2021-03-13 00:10:51 -0800

    added CSS and JS code

---
+++ app.js
@@ -0,0 +1,14 @@
+let apple = 1; 
+let orange = 2;
+const string1 = 'spongebob';
+const bool = true;
+
+if (apple == orange) {
+	console.log('they match');
+} else {
+	console.log('no luck');
+}
+
+for (const letter of string1) {
+  console.log(letter);
+}
